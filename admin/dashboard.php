48718df4bdbe6ad8fcd008142b1079b6ef3d4340
Cloudmint <76614862+Cmint22@users.noreply.github.com>
2023-06-25 23:43:15 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,9 +5,6 @@ include('includes/dbconnection.php');
 if (strlen($_SESSION['sscmsaid'] == 0)) {
     header('location:logout.php');
 } else {
-
-
-
 ?>
     <!doctype html>
     <html lang="en">
@@ -36,9 +33,6 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
 
     <body>
         <?php include_once('includes/header.php'); ?>
-
-
-
         <!-- ============================================================== -->
         <!-- Start right Content here -->
         <!-- ============================================================== -->
@@ -49,7 +43,6 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="page-title-box">
-
                             <h4 class="page-title">Dashboard</h4>
                         </div>
                     </div>
@@ -57,42 +50,43 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
 
 
                 <div class="row">
+                    <!-- Rooms -->
                     <div class="col-md-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <?php
-                            $sql0 = "SELECT * from  room";
-                            $query0 = $dbh->prepare($sql0);
-                            $query0->execute();
-                            $results0 = $query0->fetchAll(PDO::FETCH_OBJ);
-                            $totalrooms = $query0->rowCount();
-                            ?><i class="fa fa-roomtop float-right"></i>
-
+                            $sql1 = "SELECT * from  room where capacity !=0";
+                            $query1 = $dbh->prepare($sql1);
+                            $runResult = $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $totalrooms = $query1->rowCount();
+                            ?>
+                            <i class="fa fa-roomtop float-right"></i>
                             <h6 class="text-muted text-uppercase m-b-20">Total Rooms</h6>
                             <h2 class="m-b-20" data-plugin="counterup"><?php echo htmlentities($totalrooms); ?></h2>
-                            <a href="view-rooms.php"><span class="badge badge-primary"> View Detail </span></a>
+                            <a href="manage-rooms.php"><span class="badge badge-primary"> View Detail </span></a>
                         </div>
                     </div>
 
                     <div class="col-md-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <?php
-                            $sql1 = "SELECT * from  room where usability = 1";
+                            $sql1 = "SELECT * from  room where capacity !=0 and usability=1";
                             $query1 = $dbh->prepare($sql1);
                             $query1->execute();
                             $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
-                            $totalroomsavail = $query1->rowCount();
+                            $totalroomssavail = $query1->rowCount();
                             ?>
                             <i class="fa fa-roomtop float-right"></i>
-                            <h6 class="text-muted text-uppercase m-b-20">Total Room Available</h6>
-                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($totalroomsavail); ?></span></h2>
-                            <a href="view-rooms.php"><span class="badge badge-success"> View Room List </span></a>
+                            <h6 class="text-muted text-uppercase m-b-20">Total Rooms Available</h6>
+                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($totalroomssavail); ?></span></h2>
+                            <a href="manage-rooms.php"><span class="badge badge-success"> View Detail </span></a>
                         </div>
                     </div>
 
                     <div class="col-md-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <?php
-                            $sql1 = "SELECT * from  room where capacity > 80";
+                            $sql1 = "SELECT * from  room where capacity !=0 and usability=0";
                             $query1 = $dbh->prepare($sql1);
                             $query1->execute();
                             $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
@@ -101,43 +95,97 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
                             <i class="fa fa-roomtop float-right"></i>
                             <h6 class="text-muted text-uppercase m-b-20">Room Occupied</h6>
                             <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($isoccupied); ?></span></h2>
-                            <a href="view-rooms.php"><span class="badge badge-danger"> View Detail </span></a>
+                            <a href="manage-rooms.php"><span class="badge badge-danger"> View Detail </span></a>
                         </div>
                     </div>
 
+                    <!-- Equipments -->
+                    <div class="col-md-6 col-xl-4">
+                        <div class="card-box tilebox-one">
+                            <?php
+                            $sql1 = "SELECT * from equipment";
+                            $query1 = $dbh->prepare($sql1);
+                            $runResult = $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $totalequipments = $query1->rowCount();
+                            ?>
+                            <i class="fa fa-roomtop float-right"></i>
+                            <h6 class="text-muted text-uppercase m-b-20">Total Equipments</h6>
+                            <h2 class="m-b-20" data-plugin="counterup"><?php echo htmlentities($totalequipments); ?></h2>
+                            <a href="manage-equipments.php"><span class="badge badge-primary"> View Detail </span></a>
+                        </div>
+                    </div>
 
                     <div class="col-md-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <?php
-                            $sql11 = "SELECT * from  equipment ";
-                            $query11 = $dbh->prepare($sql11);
-                            $query11->execute();
-                            $results11 = $query11->fetchAll(PDO::FETCH_OBJ);
-                            $totalregstd = $query11->rowCount();
+                            $sql1 = "SELECT * from equipment where lastUserUsed is NULL";
+                            $query1 = $dbh->prepare($sql1);
+                            $runResult = $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $totalequipments = $query1->rowCount();
                             ?>
-                            <i class="fa fa-users float-right"></i>
-                            <h6 class="text-muted text-uppercase m-b-20">Total Registered Equipments</h6>
-                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($totalregstd); ?></span></h2>
-                            <a href="view-equipments.php"><span class="badge badge-danger"> View Detail </span></a>
+                            <i class="fa fa-roomtop float-right"></i>
+                            <h6 class="text-muted text-uppercase m-b-20">Total Equipments Available</h6>
+                            <h2 class="m-b-20" data-plugin="counterup"><?php echo htmlentities($totalequipments); ?></h2>
+                            <a href="manage-equipments.php"><span class="badge badge-success"> View Detail </span></a>
                         </div>
                     </div>
 
+                    <div class="col-md-6 col-xl-4">
+                        <div class="card-box tilebox-one">
+                            <?php
+                            $sql1 = "SELECT * from equipment where lastUserUsed is not NULL";
+                            $query1 = $dbh->prepare($sql1);
+                            $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $isoccupied = $query1->rowCount();
+                            ?>
+                            <i class="fa fa-roomtop float-right"></i>
+                            <h6 class="text-muted text-uppercase m-b-20">Equipment Occupied</h6>
+                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($isoccupied); ?></span></h2>
+                            <a href="manage-rooms.php"><span class="badge badge-danger"> View Detail </span></a>
+                        </div>
+                    </div>
 
+                    <!-- Room Registered -->
+                    <div class="col-md-6 col-xl-4">
+                        <div class="card-box tilebox-one">
+                            <?php
+                            $sql1 = "SELECT * from roomregisterform where reply is null";
+                            $query1 = $dbh->prepare($sql1);
+                            $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $totalequipmentsavail = $query1->rowCount();
+                            ?>
+                            <i class="fa fa-roomtop float-right"></i>
+                            <h6 class="text-muted text-uppercase m-b-20">Total Room Registered Students</h6>
+                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($totalequipmentsavail); ?></span></h2>
+                            <a href="manage-room-register-students.php"><span class="badge badge-primary"> View Detail </span></a>
+                        </div>
+                    </div>
 
-
+                    <!-- Equipment Registered -->
+                    <div class="col-md-6 col-xl-4">
+                        <div class="card-box tilebox-one">
+                            <?php
+                            $sql1 = "SELECT * from  equipmentregisterform where reply is null";
+                            $query1 = $dbh->prepare($sql1);
+                            $query1->execute();
+                            $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                            $isoccupied = $query1->rowCount();
+                            ?>
+                            <i class="fa fa-roomtop float-right"></i>
+                            <h6 class="text-muted text-uppercase m-b-20">Total Equipment Registered Students</h6>
+                            <h2 class="m-b-20"><span data-plugin="counterup"><?php echo htmlentities($isoccupied); ?></span></h2>
+                            <a href="manage-equipment-register-students.php"><span class="badge badge-primary"> View Detail </span></a>
+                        </div>
+                    </div>
                 </div>
                 <!-- end row -->
-
-
-
-
             </div> <!-- container -->
 
             <?php include_once('includes/footer.php'); ?>
-
-
-
-
         </div> <!-- End wrapper -->
 
 
