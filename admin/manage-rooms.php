80026bc88f5fedb8b146c13e1e504de5922264a2
Cloudmint <76614862+Cmint22@users.noreply.github.com>
2023-06-27 03:04:24 +0700

    Update manage-rooms.php done

--- admin/manage-rooms.php
+++ admin/manage-rooms.php
@@ -2,29 +2,28 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
+include('./QueryHandler.php');
 if (strlen($_SESSION['sscmsaid'] == 0)) {
     header('location:logout.php');
 } else {
 
-    // Code for deleting product from cart
     if (isset($_GET['delid'])) {
-        $roomID = intval($_GET['delid']);
+        $deskid = intval($_GET['delid']);
 
-        $query = $dbh->prepare("SELECT id FROM room WHERE id=:roomID and isOccupied IS NOT NULL");
-        $query->bindParam(':roomID', $roomID, PDO::PARAM_STR);
+        $query = $dbh->prepare("SELECT id FROM tbldesk WHERE id=:deskid and isOccupied is not null");
+        $query->bindParam(':deskid', $deskid, PDO::PARAM_STR);
         $query->execute();
         $results = $query->fetchAll(PDO::FETCH_OBJ);
 
         if ($query->rowCount() > 0) {
-            echo '<script>alert("Room occupied cannot be deleted")</script>';
+            echo '<script>alert("Desk occupied cannot  deleted")</script>';
         } else {
-
-            $sql = "DELETE FROM room WHERE id=:roomID";
+            $sql = "delete from tbldesk where id=:deskid";
             $query = $dbh->prepare($sql);
-            $query->bindParam(':roomID', $roomID, PDO::PARAM_STR);
+            $query->bindParam(':deskid', $deskid, PDO::PARAM_STR);
             $query->execute();
             echo "<script>alert('Data deleted');</script>";
-            echo "<script>window.location.href = 'manage-rooms.php'</script>";
+            echo "<script>window.location.href = 'manage-desks.php'</script>";
         }
     }
 
@@ -33,8 +32,7 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
     <html lang="en">
 
     <head>
-
-        <title>Student Study Center Management System | Manage Rooms</title>
+        <title>Student Study Center Mananagement System | Manage Rooms</title>
 
         <!-- DataTables -->
         <link href="../plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
@@ -57,81 +55,60 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
 
     </head>
 
-
     <body>
-
         <?php include_once('includes/header.php'); ?>
-
-
         <!-- ============================================================== -->
         <!-- Start right Content here -->
         <!-- ============================================================== -->
         <div class="wrapper">
             <div class="container">
-
-
                 <div class="row">
                     <div class="col-12">
                         <div class="card-box">
                             <h4 class="m-t-0 header-title">Manage Rooms</h4>
-
-
                             <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                 <thead>
                                     <tr>
                                         <th>#</th>
-                                        <th>Room ID</th>
-                                        <th>Laptop / Charger Socket</th>
+                                        <th>Room</th>
+                                        <th>Capacity</th>
                                         <th>Status</th>
-                                        <th>Creation Date</th>
+                                        <th>Description</th>
+                                        <th>Avaiable Time </th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
-
-
                                 <tbody>
                                     <?php
-
-                                    $sql = "SELECT * FROM room";
-                                    $query = $dbh->prepare($sql);
-                                    $query->execute();
+                                    $sql = "SELECT * from room where id!='1'";
+                                    $query = Query::executeQuery($dbh, $sql);
                                     $results = $query->fetchAll(PDO::FETCH_OBJ);
-
                                     $cnt = 1;
                                     if ($query->rowCount() > 0) {
-                                        foreach ($results as $row) {               ?>
+                                        foreach ($results as $row) { ?>
                                             <tr>
                                                 <td><?php echo htmlentities($cnt); ?></td>
-                                                <td><?php echo htmlentities($row->roomID); ?></td>
-                                                <td><?php $lapchargerscoket = $row->laptopChargerScoket;
-                                                    if ($lapchargerscoket == '') : echo "Not Available";
-                                                    else : echo "Available";
-                                                    endif; ?></td>
-
-                                                <td><?php $occupiedstatus = $row->isOccupied;
-                                                    if ($occupiedstatus == '') : echo "Available";
-                                                    else : echo "Occupied";
-                                                    endif; ?></td>
-                                                <td><?php echo htmlentities($row->postingDate); ?></td>
-                                                <td><a href="edit-room.php?did=<?php echo htmlentities($row->id); ?>" class="btn btn-primary">Edit </a> | <a href="manage-rooms.php?delid=<?php echo ($row->id); ?>" onclick="return confirm('Do you really want to Delete ?');" class="btn btn-danger btn-xs">Delete</a></td>
-
+                                                <td><?php echo htmlentities($row->id); ?></td>
+                                                <td><?php echo htmlentities($row->capacity); ?></td>
+                                                <td><?php $roomUsability = $row->usability;
+                                                    if ($roomUsability == 0) echo "Not Available";
+                                                    else echo "Available"; ?></td>
+                                                <td><?php echo htmlentities($row->description); ?></td>
+                                                <td><?php echo htmlentities($row->avaiableTime); ?></td>
+                                                <td>
+                                                    <a href="edit-desk.php?did=<?php echo htmlentities($row->id); ?>" class="btn btn-primary">Edit </a> | <a href="manage-desks.php?delid=<?php echo ($row->id); ?>" onclick="return confirm('Do you really want to Delete ?');" class="btn btn-danger btn-xs">Delete</i></a>
+                                                </td>
                                             </tr>
                                     <?php $cnt = $cnt + 1;
                                         }
                                     } ?>
-
                                 </tbody>
                             </table>
-
                         </div>
                     </div>
                 </div> <!-- end row -->
-
-
-
             </div> <!-- container -->
             <?php include_once('includes/footer.php'); ?>
-
         </div> <!-- End wrapper -->
 
 
@@ -203,5 +180,4 @@ if (strlen($_SESSION['sscmsaid'] == 0)) {
 
     </body>
 
-    </html>
-<?php } ?>
+    </html><?php }  ?>
