d2c91b1fd60b847099015cdc8e9d3eae99e470be
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-06-26 14:55:39 +0700

    make functions run better

--- admin/QueryHandler.php
+++ admin/QueryHandler.php
@@ -1,11 +1,17 @@
 <?php
 class Query
 {
-   public static function executeQuery(PDO $dbh, string $sql, string $username, string $password)
+   public static function executeQuery()
    {
+      $dbh = func_get_arg(0);
+      $sql = func_get_arg(1);
       $query = $dbh->prepare($sql);
-      $query->bindParam(':username', $username, PDO::PARAM_STR);
-      $query->bindParam(':password', $password, PDO::PARAM_STR);
+      $args = func_get_args();
+      foreach ($args as $index => $arg) {
+         if ($index >= 2) {
+            $query->bindParam($arg[0], $arg[1], PDO::PARAM_STR);
+         }
+      }
       $query->execute();
       return $query;
    }
