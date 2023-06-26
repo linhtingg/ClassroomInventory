16af4bd45523bd14417714fada777823bdb1d27b
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-06-26 16:43:23 +0700

    type hints for function

--- admin/QueryHandler.php
+++ admin/QueryHandler.php
@@ -1,16 +1,11 @@
 <?php
 class Query
 {
-   public static function executeQuery()
+   public static function executeQuery(PDO $dbh, string $sql, array ...$bindParams)
    {
-      $dbh = func_get_arg(0);
-      $sql = func_get_arg(1);
       $query = $dbh->prepare($sql);
-      $args = func_get_args();
-      foreach ($args as $index => $arg) {
-         if ($index >= 2) {
-            $query->bindParam($arg[0], $arg[1], PDO::PARAM_STR);
-         }
+      foreach ($bindParams as $index => $arg) {
+         $query->bindParam($arg[0], $arg[1], PDO::PARAM_STR);
       }
       $query->execute();
       return $query;
