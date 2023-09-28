9bf524adf061a10854cf7f9688460f2daa4014ec
Hoàng Linh <116622444+linhtingg@users.noreply.github.com>
2023-09-28 13:01:03 +0700

    Heroku deploy

--- helper/QueryHandler.php
+++ helper/QueryHandler.php
@@ -1,10 +1,10 @@
 <?php
 class Query
 {
-   private static $DB_HOST = 'localhost';
-   private static $DB_USER = 'root';
-   private static $DB_PASS = '';
-   private static $DB_NAME = 'class';
+   private static $DB_HOST = 'us-east-1.rds.amazonaws.com';
+   private static $DB_USER = 'a8nhk50ikm8wvmdq';
+   private static $DB_PASS = 'imuwf47fsappj30x';
+   private static $DB_NAME = 'class2';
    private static $dbh = null;
    /**
     * Execute SQL query and return the query.
